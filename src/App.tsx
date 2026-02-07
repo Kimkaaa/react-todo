ef4d6836845f45270bb66e1661c62c74ff7c8867
kimkaaa <yami78999@gmail.com>
2026-02-08 02:54:56 +0900

    할 일 목록 localStorage 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoEditor from "./components/TodoEditor";
 import TodoHeader from "./components/TodoHeader";
 import TodoList from "./components/TodoList";
 
 export default function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(() =>
+    JSON.parse(localStorage.getItem('todos') || '[]'));
   const addTodo = (title: string) => {
     setTodos((todos) => [
       ...todos,
@@ -30,6 +31,11 @@ export default function App() {
       todos.map((todo) => (todo.id === id ? { ...todo, title } : todo))
     );
   };
+  // todos 상태 값이 변경될 때마다 localStorage에 저장
+  // useEffect 훅으로 업데이트, 사이드 이펙트로 처리
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   return (
     <div className="todo">
       <TodoHeader />
